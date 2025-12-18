3e363cf9c0005ec9b16c62ce8c962f4868464cd2
jungamer-64 <79193245+jungamer-64@users.noreply.github.com>
2025-12-19 00:35:08 +0900

    deprecate(ps2): mark ps2_init deprecated and propagate to io re-export; update DEPRECATIONS.md

--- kernel/src/io/hid/mod.rs
+++ kernel/src/io/hid/mod.rs
@@ -58,6 +58,7 @@ pub use ps2::Ps2Controller;
 pub use ps2::commands as ps2_commands;
 
 // Functions
+#[deprecated(note = "ps2_init is deprecated; prefer registering the PS/2 driver via `driver_registry::register_driver(Box::new(Ps2Driver::new()))` or calling `crate::io::hid::ps2::init()` directly; this free function will be removed in a future release.")]
 pub use ps2::init as ps2_init;
 pub use ps2::kbd_commands as ps2_kbd_commands;
 pub use ps2::keyboard_interrupt_handler;
